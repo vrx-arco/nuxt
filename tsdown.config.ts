2ad4a35cf6ffb41768385b3bf1f54f93ad0ec611
whitekite <1075790909@qq.com>
2025-05-13 23:24:53 +0800

    chore(deps): update dev deps

---
+++ tsdown.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'tsdown'
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  sourcemap: false,
+  clean: true,
+  dts: true,
+  target: 'node16',
+  treeshake: true,
+  format: ['esm'],
+  publint: true,
+  unused: {
+    ignore: ['nuxt'],
+  },
+})
